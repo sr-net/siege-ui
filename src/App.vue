f7c6f8892ce9cc133a0a4c3e6b5986f25be47684
BeeeQueue <adam.lj.haglund@gmail.com>
2019-12-08 00:11:17 +0100

    prettier write

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,20 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
-  }
-});
+    HelloWorld,
+  },
+})
 </script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
