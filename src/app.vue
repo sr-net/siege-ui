e00fc99deafada7f311ac81fc57deb639d61d553
Adam <adam.lj.haglund@gmail.com>
2019-12-08 02:49:34 +0100

    minor changes

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
@@ -19,11 +20,5 @@ export default Vue.extend({
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
