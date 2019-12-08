0ca49942a3c004c2a964b27cff4c9cf963682b88
BeeeQueue <adam.lj.haglund@gmail.com>
2019-12-08 20:08:57 +0100

    add title

---
+++ src/components/title.vue
@@ -0,0 +1,96 @@
+<template>
+  <div ref="container" class="title-container">
+    <div class="title">{{ state.currentTitle }}</div>
+
+    <div class="title">Loading...</div>
+
+    <div class="title">{{ title }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  createComponent,
+  onMounted,
+  reactive,
+  ref,
+  watch,
+} from '@vue/composition-api'
+
+export default createComponent({
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const container = ref<HTMLDivElement>(null)
+
+    const state = reactive({
+      currentTitle: props.title,
+      height: 70,
+    })
+
+    onMounted(() => {
+      state.height = container.value!.getBoundingClientRect().height ?? 70
+    })
+
+    watch(() => {
+      if (props.loading) {
+        container.value?.scrollTo({ top: 0 })
+
+        container.value?.scrollTo({
+          top: state.height,
+          behavior: 'smooth',
+        })
+      }
+
+      if (!props.loading) {
+        state.currentTitle = props.title
+        container.value?.scrollTo({
+          top: state.height * 2,
+          behavior: 'smooth',
+        })
+      }
+    })
+
+    return { state, container }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+@import '../variables';
+
+.title-container {
+  display: flex;
+  flex-direction: column;
+  height: 70px;
+  width: 100%;
+  overflow: hidden;
+  background: $bg200;
+  color: $text;
+
+  font-weight: 600;
+  font-size: 25px;
+
+  @include notMobile {
+    height: 80px;
+    font-size: 35px;
+  }
+
+  & > .title {
+    flex-shrink: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
