424c7ee91e4af4fb724aee9e6cd2567b09a69c2b
Adam <adam@haglund.dev>
2021-06-04 20:11:56 +0200

    disable not working stuff

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import Script2 from 'vue-script2'
-import Ads from 'vue-google-adsense'
+// import Script2 from 'vue-script2'
+// import Ads from 'vue-google-adsense'
 import App from './app.vue'
 
 const app = createApp(App)
 
-app.use(Script2)
-app.use(Ads.Adsense)
+// app.use(Script2)
+// app.use(Ads.Adsense)
 
 app.mount('#app')
