86fd983786c8ee81d57af259c06a922ab0d77861
Adam <adam@haglund.dev>
2021-06-04 21:36:55 +0200

    remove unused file

--- src/shims-deps.d.ts
+++ src/shims-deps.d.ts
@@ -9,19 +9,3 @@ declare module "*.png" {
   const content: string
   export default content
 }
-
-declare module "vue-script2" {
-  import { PluginFunction } from "vue"
-  const content: PluginFunction<any>
-  export default content
-}
-
-declare module "vue-google-adsense" {
-  import { PluginFunction } from "vue"
-  const content: {
-    Adsense: PluginFunction<any>
-    InArticleAdsense: PluginFunction<any>
-    InFeedAdsense: PluginFunction<any>
-  }
-  export default content
-}
