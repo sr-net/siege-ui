6ddcd8b4ed2c423b7830a440616f10cc4c644edb
Adam <adam@haglund.dev>
2022-12-02 16:37:27 +0100

    self-host webfonts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue"
 import tsconfig from "vite-tsconfig-paths"
 import { esbuildPluginBrowserslist } from "esbuild-plugin-browserslist"
 import browserslist from "browserslist"
+import webfont from "vite-plugin-webfont-dl"
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
 
   plugins: [
     vue(),
+    webfont(),
     esbuildPluginBrowserslist(browserslist()),
     tsconfig({
       extensions: [".vue"],
